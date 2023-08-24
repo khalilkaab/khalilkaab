88fc4b0a0ed5fb30ceeb86844a53039d54a4b384
dgtbox <jetaimesouhaila@gmail.com>
2023-08-25 00:45:51 +0100

    change to html

--- thepitabar/index.php
+++ thepitabar/index.php
@@ -28,9 +28,61 @@
       <!-- Page Header-->
       <header class="page-head">
         <!-- RD Navbar-->
-        <?php
-            include("navbar.php")
-            ?>
+        <div class="rd-navbar-wrap rd-navbar-minimal">
+          <nav class="rd-navbar" data-layout="rd-navbar-fixed" data-sm-layout="rd-navbar-fixed" data-md-device-layout="rd-navbar-fixed" data-lg-layout="rd-navbar-static" data-lg-device-layout="rd-navbar-static" data-stick-up-clone="false" data-md-stick-up-offset="100px" data-lg-stick-up-offset="100px">
+            <div class="container container-fluid">
+              <div class="rd-navbar-inner">
+                <!-- RD Navbar Panel-->
+                <div class="rd-navbar-panel">
+                  <!-- RD Navbar Toggle-->
+                  <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                  <!-- RD Navbar Brand-->
+                  <a class="rd-navbar-brand brand" href="index.php">
+                  <div class="brand-logo">
+                    <img src="images/logo.png" alt="" srcset="">
+                  </div></a>
+                </div>
+                <!-- RD Navbar Nav-->
+                <div class="rd-navbar-nav-wrap">
+                  <!-- RD Navbar Nav-->
+                  <!-- RD Navbar Nav-->
+                  <ul class="rd-navbar-nav" style="margin-left: 15%;">
+                    <li class="active"><a class="navbar-icon restaurant-icon-19" href="#">Menu</a>
+                      <!-- RD Navbar Dropdown-->
+                      <ul class="rd-navbar-dropdown menu-img-wrap">
+                        <li class="menu-img"><a href="menu-single.html"><img src="images/menu-food-01.png" alt="" width="88" height="60"><span>Pizzas</span></a></li>
+                        <li class="menu-img"><a href="menu-single.html"><img src="images/menu-food-02.png" alt="" width="88" height="60"><span>Burgers</span></a></li>
+                        <li class="menu-img"><a href="menu-single.html"><img src="images/menu-food-03.png" alt="" width="88" height="60"><span>Drinks</span></a></li>
+                        <li class="menu-img"><a href="menu-single.html"><img src="images/menu-food-04.png" alt="" width="88" height="60"><span>Tosts</span></a></li>
+                        <li class="menu-img"><a href="menu-single.html"><img src="images/menu-food-05.png" alt="" width="88" height="60"><span>Salads</span></a></li>
+                        <li class="menu-img"><a href="menu-single.html"><img src="images/menu-food-06.png" alt="" width="88" height="60"><span>desserts</span></a></li>
+                      </ul>
+                    </li>
+                    <li><a class="navbar-icon thin-icon-tag" href="#">News</a>
+                      <ul class="rd-navbar-dropdown">
+                        <li><a href="blog-classic.html">Classic Blog</a></li>
+                        <li><a href="blog-grid.html">Grid Blog</a></li>
+                        <li><a href="blog-masonry.html">Masonry Blog</a></li>
+                        <li><a href="blog-modern.html">Modern Blog</a></li>
+                        <li><a href="blog-post.html">Blog Post</a></li>
+                      </ul>
+                    </li>
+                    <li><a class="navbar-icon thin-icon-map-marker" href="contacts.html">Contacts</a></li>
+                  </ul>
+
+                  <!-- RD Navbar Shop-->
+                  <ul class="rd-navbar-shop list-inline">
+                    <li><a class="unit unit-horizontal unit-middle unit-spacing-xxs link-default" href="tel:0707040302">
+                        <div class="unit-left"><span class="icon icon-md icon-primary thin-icon-phone-call text-middle"></span></div>
+                        <div class="unit-body">
+                          <address class="contact-info"><span class="text-bold big">+212(0)707040302</span></address>
+                        </div></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
       </header>
       <!-- Page Content-->
       <main class="page-content">
